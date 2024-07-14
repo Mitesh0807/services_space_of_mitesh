0030cbd1a35f5d723b8834d0f5c372a0e14e7a4d
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-07-14 20:07:33 +0530

    "Updated dependencies and imports in NestJS projects, added pino-pretty and logger modules, and modified main.ts files."

--- apps/services_space_of_mitesh/src/main.ts
+++ apps/services_space_of_mitesh/src/main.ts
@@ -3,20 +3,20 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  app.useLogger(app.get(Logger));
 }
 
 bootstrap();
