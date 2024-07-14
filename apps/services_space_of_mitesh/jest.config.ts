37f4b195ee7a42eca190a19071fe640a492e5916
Mitesh0807 <miteshsavaliya7@gmail.com>
2024-07-14 18:11:32 +0530

    Initial commit

---
+++ apps/services_space_of_mitesh/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'services_space_of_mitesh',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/services_space_of_mitesh',
+};
